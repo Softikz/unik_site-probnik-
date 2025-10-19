3036137111b556a46814fd61bf67f265427084a1
Unik <sergeymosin04@gmail.com>
2025-10-19 19:19:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+#маршрут для подтверждения Яндекса
+@app.route('/yandex_ee4e99d668bda113.html')
+def yandex_verify():
+    return send_from_directory('.', 'yandex_ee4e99d668bda113.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
